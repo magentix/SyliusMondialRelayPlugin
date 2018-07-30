fc924beea97ba4cd89abe2dc28536853b16f9074
magentix <mvion@magentix.fr>
2018-07-30 10:01:25 +0200

    GPL v3 Licence added

--- src/MagentixMondialRelayPlugin.php
+++ src/MagentixMondialRelayPlugin.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * @author    Matthieu Vion
+ * @copyright 2018 Magentix
+ * @license   https://opensource.org/licenses/GPL-3.0 GNU General Public License version 3
+ * @link      https://github.com/magentix/mondial-relay-plugin
+ */
 declare(strict_types=1);
 
 namespace MagentixMondialRelayPlugin;
